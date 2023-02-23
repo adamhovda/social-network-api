9e7c950d818708f0a64f8c7d4ab137ddaa07c4db
adam hovda <adamhovda@gmail.com>
2023-02-22 20:09:17 -0700

    added models

---
+++ models/Reaction.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {type: Schema.Types.ObjectId, default: null },
+        reactionBody: {type: String, required: true, max: 280},
+        username: {type: String, required: true},
+        // TODO Use a getter method to format the timestamp on query
+        createdAt: {type: Date, default: Date.now, },
+    }
+)
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
